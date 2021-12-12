13a23b12f52a3ad2498f571adee4818fba9146c8
PushpenderKumar7505 <81156361+PushpenderKumar7505@users.noreply.github.com>
2021-12-12 12:32:17 +0530

    Add files via upload

---
+++ Test78.java
@@ -0,0 +1,14 @@
+// wrapper class`
+public class Test78 {
+    public static void main(String[] args) {
+        Integer obj = Integer.valueOf(45); // boxing
+        Integer obj1 = Integer.valueOf(60);
+        System.out.println(obj == obj);
+        Test78 obj3 = new Test78();
+        Test78 obj4 = new Test78();
+        System.out.println(obj3 == obj4);
+    }
+Integer i1 =40; // autoboxing
+    int i2 = i1; //autoboxing
+
+}
